c64124bc277a30c6860d33c84345d6d2880ab222
Jabeed Ahmed <jabeedahmed0@gmail.com>
2022-11-02 10:47:20 +0300

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Qatar Executive on 11/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
